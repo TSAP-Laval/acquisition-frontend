13b7b752d95074fd6d2a3a54998ea95766383e94
Mehdi Laribi <m.laribi@hotmail.com>
2017-01-19 15:58:31 -0500

    try fix

--- src/Manage/Actions.tsx
+++ src/Manage/Actions.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
-import {Button} from "react-bootstrap";
-
+import {Button, Alert} from "react-bootstrap";
+import * as $ from "jquery"
 //require("bootstrap-sass/assets/stylesheets/_bootstrap.scss");
 
 export interface ILayoutProps {}
@@ -10,13 +10,93 @@ export interface ILayoutState {}
 
 export default class Actions extends React.Component<ILayoutProps, ILayoutState> {
     render() {
-        return (
+        
+        function Reussi() {
+            alert("Ajout réussi")
+        }
 
-                <div className="bootstrap-iso">
+
+      function SubmitAction(){
+          var http = new XMLHttpRequest();
+          var url = "http://localhost:3000/api/PostActionType";
+
+         
+        var text = '{'
+       +'"Nom" :' + '"' +$('#action_name').val() + '"'+','
+       +'"Description" : '+ '"' +$('#action_desc').val() + '"'
+       +'}';
+
+       var stringg = JSON.stringify(text);
+
+          console.log(text);
+          console.log(url);
+
+        http.open('POST', url, true);
+
+        http.setRequestHeader("Content-type", "application/json");
+
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4) {
+                console.log("test");
+            
+                
+                console.log("response text : " +http.responseText);
+                console.log("response" + http.getResponseHeader);
+                console.log("response :" + http.response);
+                console.log("response :" + http.responseURL);
+            }else{
+                console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+        }
+        console.log(stringg);
+        
+        http.send(stringg);
+
+      }
+        
+
+        return (
+                    
+
                     <div className="container-fluid">
                         <div className="row">
                             <div className="col-md-6 col-sm-6 col-xs-12">
-                                <form method="post" title="Actions :">
+
+                                <h1>Action types :</h1>
+
+                                <table className="table table-bordered table-hover" id="action_table">
+                                    <thead>
+                                        <tr >
+                                            <th className="text-center">
+                                                Nom
+                                            </th>
+                                            <th className="text-center">
+                                                Description
+                                            </th>
+                                            <th className="text-center">
+                                                Actions
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr id='action1'>
+                                            <td>
+                                                Passe
+                                            </td>
+                                            <td>
+                                                Une belle passe
+                                            </td>
+                                            <td>
+                                            <button className="btn btn-default btn-warning">Modifier</button>
+                                            <button className="btn btn-danger btn-default">Supprimer</button>
+                                            </td>
+                                        </tr>
+                                        <tr id='addr1'></tr>
+                                    </tbody>
+                                </table>
+
+
+                                <form method="post" title="Actions :" id="actionForm">
                                     <div className="form-group ">
                                         <label className="control-label requiredField" htmlFor="action_name">
                                         Nom de l'action :
@@ -24,13 +104,13 @@ export default class Actions extends React.Component<ILayoutProps, ILayoutState>
                                             *
                                         </span>
                                         </label>
-                                        <input className="form-control" id="action_name" name="action_name" type="text"/>
+                                        <input className="form-control" id="action_name" name="Nom" type="text"/>
                                     </div>
                                     <div className="form-group ">
                                         <label className="control-label " htmlFor="action_desc">
                                         Description :
                                         </label>
-                                        <textarea className="form-control" cols={40} id="action_desc" name="action_desc" rows={10}></textarea>
+                                        <textarea className="form-control" cols={40} id="action_desc" name="Description" rows={10}></textarea>
                                     </div>
                                         <div className="form-group ">
                                         <label className="control-label requiredField" htmlFor="mov_type">
@@ -40,11 +120,14 @@ export default class Actions extends React.Component<ILayoutProps, ILayoutState>
                                         </span>
                                         </label>
                                         <select className="select form-control" id="mov_type" name="mov_type">
+                                            <option value="Defensive"></option>
+                                            <option value="Offensive"></option>
+                                            <option value="Neutre"></option>
                                         </select>
                                     </div>
                                     <div className="form-group">
                                         <div>
-                                        <Button bsStyle="primary">
+                                        <Button bsStyle="primary" onClick={SubmitAction}>
                                             Submit
                                         </Button>
                                         </div>
@@ -53,7 +136,6 @@ export default class Actions extends React.Component<ILayoutProps, ILayoutState>
                             </div>
                         </div>
                     </div>
-                </div>
         );
     }
 }
