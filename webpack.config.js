4155ea7492e2cd30beb396f0ec9ba22632d29dc9
Laurent Leclerc Poulin <laurentleclercpoulin@gmail.com>
2017-03-12 17:21:28 -0400

    Updated deployment and added NODE_ENV for prod feature

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var debug = !(process.env.ENV === 'production');
+var debug = !(process.env.NODE_ENV === 'production');
 console.log("DEBUG: " + debug.valueOf());
 
 var webpack = require('webpack');
