d49acc328027a67184b7de59872b607ef103ae98
Laurent Leclerc Poulin <laurentleclercpoulin@gmail.com>
2017-03-12 23:00:55 -0400

    fix tests errors and stating working on them

---
+++ karma.conf.js
@@ -0,0 +1,88 @@
+var webpackConfig = require('./webpack.config');
+var path = require('path');
+const autoprefixer = require('autoprefixer')
+
+const sassLoaders = [
+    'css-loader',
+    'postcss-loader',
+    'sass-loader?includePaths[]=' + path.resolve(__dirname, 'node_modules')
+];
+
+module.exports = function (config) {
+  config.set({
+    basePath: path.resolve(__dirname, './'),
+    files: [
+      'tests/tests.webpack.js'
+    ],
+    preprocessors: {
+      'tests/tests.webpack.js': [ 'webpack', 'sourcemap', 'coverage' ]
+    },
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+          loaders: [
+              {
+                  test: /\.(ts|tsx)$/,
+                  exclude: path.resolve(__dirname, 'node_modules'),
+                  loader: 'ts-loader'
+            },
+            {
+              test: /\.scss$/,
+              loaders: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            //Configuration required by enzyme
+            {
+                test: /\.json$/,
+                loader: 'json'
+            }
+          ]
+      },
+      resolve: {
+          //Added .json extension required by cheerio (enzyme dependency)
+          extensions: ['', '.js', '.ts', '.tsx', '.json', '.scss', '.css']
+      },
+      //Configuration required by enzyme
+      externals: {
+        'react/addons': true,
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': 'window',
+        'config': JSON.stringify(webpackConfig.debug ? {
+            serverURL: "http://localhost:3000/api"
+        } : {
+            serverURL: "http://67.205.146.224:3000/api"
+        })
+      },
+      postcss: [
+        autoprefixer({
+            browsers: ['last 2 versions']
+        })
+    ]
+    },
+    webpackServer: {
+        noInfo: true
+    },
+    coverageReporter: {
+      type : 'text',
+      dir : 'coverage/',
+      file : 'coverage.txt'
+    },
+    frameworks: ['mocha', 'chai', 'sinon'],
+    reporters: ['progress', 'coverage'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: true,
+    concurrency: Infinity,
+    plugins: [
+            require('karma-chrome-launcher'),
+            require('karma-sourcemap-loader'),
+            require('karma-webpack'),
+            require('karma-chai'),
+            require('karma-mocha'),
+            require('karma-sinon'),
+            require('karma-coverage'),
+        ]
+  })
+}
