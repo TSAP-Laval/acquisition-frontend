71b0da7b8a56f3ea20d337641b1e1f9d6eeb48d4
Alex <Alexandre8716437@hotmail.com>
2017-03-17 12:19:24 -0700

    conflits

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,9 @@
 import * as React    from "react";
 import * as ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
-<<<<<<< HEAD
-import { Home } from "./pages/Home"
-import { Upload } from "./pages/upload"
-import { Edit }   from "./pages/edit"
-=======
-<<<<<<< HEAD
-
-
-
-import { Home } from "./pages/Home"
-import { Upload } from "./pages/upload"
-import { Edit } from "./pages/edit"
-=======
 import { Home }   from "./pages/Home"
 import { Upload } from "./pages/Upload"
 import { Edit }   from "./pages/Edit"
->>>>>>> da3ed6bcaa35ee7eebd9430506998e880c8f5749
->>>>>>> dev
 
 ReactDOM.render(
     <Router history={hashHistory}>
