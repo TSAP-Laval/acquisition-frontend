737f3ca416e09d32fb0a6d460facf436583be6dd
Alexandrehebertvincent <alexandre.hebertvincent@gmail.com>
2017-03-20 10:18:14 -0400

    Merge

--- src/components/Manage/Players.tsx
+++ src/components/Manage/Players.tsx
@@ -4,39 +4,33 @@ import { Button, ButtonToolbar } from "react-bootstrap";
 import * as manageActions        from "../../actions/ManageActions";
 import store                     from "../../stores/PlayersStore";
 
+
+
 export interface ILayoutProps {}
 export interface ILayoutState {}
 
-var numJoueur = "";
-var TableauJoueurs:any = [];
-var TableauJoueursId:any = [];
+var numJoueur ="";
+var TableauJoueurs:any=[];
+var TableauJoueursId:any=[];
 
 
 
 export default class Players extends React.Component<ILayoutProps, ILayoutState> {
-    constructor (props: any) {
-        super(props);
-        this.state = {
-        }
-    }
-
-    componentWillMount() {
-        manageActions.getSportJoueur();
-        manageActions.getJoueur();
-        manageActions.getNiveauJoueur();
-        manageActions.getEquipesJoueur();
-        store.on("change",() =>{
-            this.LstJoueurs();
-            this.RemplirSelect();
-        });
-    }
-
-    LstJoueurs() {
+componentWillMount(){
+    manageActions.getSportJoueur();
+    manageActions.getJoueur();
+    manageActions.getNiveauJoueur();
+    manageActions.getEquipesJoueur();
+    store.on("change",() =>{
+        this.LstJoueurs();
+        this.RemplirSelect();
+    })  
+}
+LstJoueurs(){
     this.ClearDomElement("tbody");
-        var AllJoueurs= store.GetAllJoueurs();
-        var datastringify =JSON.stringify(AllJoueurs);
-        var tabJson = JSON.parse(datastringify);
-
+    var AllJoueurs= store.GetAllJoueurs();
+    var datastringify =JSON.stringify(AllJoueurs);
+    var tabJson = JSON.parse(datastringify);		
         //Rentre le id et le nom de l'action dans le tableau correspondant
         for(var i = 0; i < tabJson.length; i++) {	
             var data =tabJson[i];        	 
@@ -52,7 +46,8 @@ export default class Players extends React.Component<ILayoutProps, ILayoutState>
             tdEmail.innerHTML=data.Email;
             var btnModifier = document.createElement("button") as HTMLButtonElement;
             btnModifier.innerHTML="modifier";
-            btnModifier.onclick=this.ModifJoueur.bind(this,i,data.ID);
+            //btnModifier.onclick=this.RightClick.bind(this);
+            btnModifier.value=data.ID;
             x.appendChild(tdNom);
             x.appendChild(tdPrenom);
             x.appendChild(tdNumero);
@@ -61,77 +56,27 @@ export default class Players extends React.Component<ILayoutProps, ILayoutState>
             doc.appendChild(x);
         }
 }
-ModifJoueur(i:any, id:any){
-    var doc = document.getElementById("action_table") as HTMLTableElement;
-    var t = doc.rows[i+1];
-    var nomjoueur= t.cells[0].innerHTML;
-    var inputNom = document.getElementById("Nom") as HTMLInputElement;
-    inputNom.value=nomjoueur;
-    var prenomjoueur= t.cells[1].innerHTML;
-    var inputPrenom = document.getElementById("Prenom") as HTMLInputElement;
-    inputPrenom.value=prenomjoueur;
-    var numeroJoueur= t.cells[2].innerHTML;
-    var inputNumero = document.getElementById("Numero") as HTMLInputElement;
-    inputNumero.value=numeroJoueur;
-    var emailJoueur= t.cells[3].innerHTML;
-    var inputEmail = document.getElementById("Email") as HTMLInputElement;
-    inputEmail.value=emailJoueur;
-    var btnSubmit = document.getElementById("btnSubmit") as HTMLButtonElement;
-    btnSubmit.value="Modifier";
-    var inputID = document.getElementById("ID") as HTMLInputElement;
-    inputID.value=id;
-    
-}
 ClearDomElement(nom:string){
     var doc = document.getElementById(nom);
     while (doc.hasChildNodes()) {
     doc.removeChild(doc.lastChild);
     }
-
-    ModifJoueur(i:any, id:any) {
-        var doc = document.getElementById("action_table") as HTMLTableElement;
-        var t = doc.rows[i+1] as HTMLTableRowElement;
-        var nomjoueur= t.cells[0].innerHTML;
-        var inputNom = document.getElementById("Nom") as HTMLInputElement;
-        inputNom.value=nomjoueur;
-        var prenomjoueur= t.cells[1].innerHTML;
-        var inputPrenom = document.getElementById("Prenom") as HTMLInputElement;
-        inputPrenom.value=prenomjoueur;
-        var numeroJoueur= t.cells[2].innerHTML;
-        var inputNumero = document.getElementById("Numero") as HTMLInputElement;
-        inputNumero.value=numeroJoueur;
-        var emailJoueur= t.cells[3].innerHTML;
-        var inputEmail = document.getElementById("Email") as HTMLInputElement;
-        inputEmail.value=emailJoueur;
-        var btnSubmit = document.getElementById("btnSubmit") as HTMLButtonElement;
-        btnSubmit.value="Modifier";
-        var inputID = document.getElementById("ID") as HTMLInputElement;
-        inputID.value=id;
-    }
-
-    ClearDomElement(nom:string){
-        var doc = document.getElementById(nom);
-        while (doc.hasChildNodes()) {
-            doc.removeChild(doc.lastChild);
-        }
-    }
-
-    RemplirSelect(){
-        this.ClearDomElement("equipe");
-        var allSport= store.GetAllequipeJoueur();
-        var datastringify =JSON.stringify(allSport);
-        var tabJson = JSON.parse(datastringify);		
+}
+RemplirSelect(){
+    this.ClearDomElement("equipe");
+    var allSport= store.GetAllequipeJoueur();
+    var datastringify =JSON.stringify(allSport);
+    var tabJson = JSON.parse(datastringify);		
         //Rentre le id et le nom de l'action dans le tableau correspondant
         for(var i = 0; i < tabJson.length; i++) {
-            var data =tabJson[i];
-            var leNiv= store.getNiveauNom(data.CategoryID);
-            var doc = document.getElementById("equipe");
-            var x = document.createElement("OPTION") as HTMLInputElement;
-            x.innerHTML=data.Name + "  "+ leNiv;
-            x.value=data.ID;
-            doc.appendChild(x);
+        var data =tabJson[i];
+        var leNiv= store.getNiveauNom(data.CategoryID)
+        var doc = document.getElementById("equipe");
+        var x = document.createElement("OPTION") as HTMLInputElement;
+        x.innerHTML=data.Name + "  "+ leNiv;
+        x.value=data.ID;
+        doc.appendChild(x);
         }
-
 }
 sendFormData(e: React.MouseEvent<HTMLInputElement>) {
     e.preventDefault()
@@ -147,19 +92,11 @@ sendFormData(e: React.MouseEvent<HTMLInputElement>) {
     let _EmailJoueur = document.getElementById("Email")as HTMLInputElement
     var emailJoueur= _EmailJoueur.value
     let _EquipeSelect = document.getElementsByName("equipe")[0] as HTMLSelectElement
-    var optEquipe = _EquipeSelect.options[_EquipeSelect.selectedIndex];
+    var optEquipe = _EquipeSelect.options[_EquipeSelect.selectedIndex] as HTMLOptionElement;
 
       //Preparation du json que l'on va envoyer au server
       
-  
-    var btnSubmit = document.getElementById("btnSubmit") as HTMLButtonElement;
-   
-    if( btnSubmit.value=="Modifier")
-    {
-         var inputID = document.getElementById("ID") as HTMLInputElement;
-        var IdJoueur= inputID.value;
-        var text = '{'
-        +'"ID" :'+ '"'+ IdJoueur+'",'
+    var text = '{'
         +'"Lname" :'+ '"'+ nomjoueur+'",'
         +'"Fname" :'+ '"'+prenomjoueur + '",'
         +'"Number" : '+numerojoueur + ','
@@ -170,52 +107,35 @@ sendFormData(e: React.MouseEvent<HTMLInputElement>) {
         +'"TokenConnexion" : "test",'
         +'"EquipeID" : '+ '"'+ optEquipe.value + '"'
         +'}'
-         manageActions.putJoueur(text,IdJoueur);
-    }
-    else
-    {
-        var text = '{'
-        +'"Lname" :'+ '"'+ nomjoueur+'",'
-        +'"Fname" :'+ '"'+prenomjoueur + '",'
-        +'"Number" : '+numerojoueur + ','
-        +'"Email" : '+ '"'+emailJoueur + '",'
-        +'"PassHash" : "test22" ,'
-        +'"TokenInvitation" : "test" ,'
-        +'"TokenReinitialisation" : "test ",'
-        +'"TokenConnexion" : "test",'
-        +'"EquipeID" : '+ '"'+ optEquipe.value + '"'
-        +'}'
+
         manageActions.postJoueur(text);
-    }
 
-
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 col-sm-6 col-xs-12">
-                        <h3>Les joueurs :</h3>
+}
+render() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 col-sm-6 col-xs-12">
+                    <h3>Les joueurs :</h3>
                         <div id="TableSelect">
-                            <table className="table table-bordered table-hover" id="action_table">
-                                <thead>
-                                    <tr>
+                        <table className="table table-bordered table-hover" id="action_table">
+                            <thead>
+                                    <tr >
                                         <th className="text-center">
-                                            Nom
-                                        </th>
-                                        <th className="text-center">
-                                            Prenom
-                                        </th>
-                                        <th className="text-center">
-                                            Numero
-                                        </th>
-                                        <th className="text-center">
-                                            Email
-                                        </th> 
-                                        <th className="text-center">
-                                            Action
-                                        </th>                                                                               
+                                        Nom
+                                    </th>
+                                    <th className="text-center">
+                                        Prenom
+                                    </th>
+                                    <th className="text-center">
+                                        Numero
+                                    </th>
+                                    <th className="text-center">
+                                        Email
+                                    </th> 
+                                     <th className="text-center">
+                                        Action
+                                    </th>                                                                               
                                     </tr>
                             </thead>
                             <tbody id="tbody">
@@ -233,11 +153,14 @@ sendFormData(e: React.MouseEvent<HTMLInputElement>) {
                 <label htmlFor="Email">Email</label>
                 <input type="text"id="Email" name="Email"/> 			
                 <label htmlFor="equipe">Équipe</label>                  
-                <select id="equipe" name="equipe"></select><br></br>   
-                <input type="hidden" id="ID"/> 
-                <input type="submit" value="Ajouter" id="btnSubmit"  />                
+                <select id="equipe" name="equipe"></select><br></br>    
+                <input type="submit" value="Submit"  />                
             </form>
             </div>
+        </div> 
+        </div>
+            
         );
     }
 }
+
