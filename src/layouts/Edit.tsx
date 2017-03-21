771dfd778214c8dcf175154ec62c72a4cca74e25
Alexandrehebertvincent <alexandre.hebertvincent@gmail.com>
2017-03-21 12:32:50 -0400

    Fleche + action finale

--- src/layouts/Edit.tsx
+++ src/layouts/Edit.tsx
@@ -6,6 +6,7 @@ import VideoStore from "../stores/VideoPlayerStore";
 import UploaderStore from "../stores/UploaderStore";
 import * as Actions from "../actions/EditAction";
 import Header from "./Header"
+import * as Motion from 'react-motion';
 
 require('../sass/Layout.scss');
 
@@ -13,41 +14,43 @@ require('../sass/Layout.scss');
 export interface ILayoutProps {}
 export interface ILayoutState {
   _lesJoueurs: any,
+  _formState: any
 }
 
 // Variable global pour avoir le numero du joueur
 var numJoueur = 0;
+var fleche: [any, any] = [[], []];
  /**
-     * rows représente les joueurs sur le terrain.
-     * 
-     * Liste des index:
-     * 
-     *  rows[0] -> Ligne défensive
-     *    rows[0][0] -> Gauche
-     *    rows[0][1] -> Centre
-     *    rows[0][2] -> Droite
-     * 
-     *  rows[1] -> Ligne de centre
-     *    rows[1][0] -> Gauche
-     *    rows[1][1] -> Centre
-     *    rows[1][2] -> Droite
-     * 
-     *  rows[2] -> Ligne offensive
-     *    rows[2][0] -> Gauche
-     *    rows[0][2] -> Centre
-     *    rows[0][3] -> Droite
-     */
-    var rows: any = [
-                      [
-                        [], [], []
-                      ], 
-                      [
-                        [], [], []
-                      ], 
-                      [
-                        [], [], []
-                      ]
-                    ];
+   * rows représente les joueurs sur le terrain.
+   * 
+   * Liste des index:
+   * 
+   *  rows[0] -> Ligne défensive
+   *    rows[0][0] -> Gauche
+   *    rows[0][1] -> Centre
+   *    rows[0][2] -> Droite
+   * 
+   *  rows[1] -> Ligne de centre
+   *    rows[1][0] -> Gauche
+   *    rows[1][1] -> Centre
+   *    rows[1][2] -> Droite
+   * 
+   *  rows[2] -> Ligne offensive
+   *    rows[2][0] -> Gauche
+   *    rows[0][2] -> Centre
+   *    rows[0][3] -> Droite
+   */
+  var rows: any = [
+                    [
+                      [], [], []
+                    ], 
+                    [
+                      [], [], []
+                    ], 
+                    [
+                      [], [], []
+                    ]
+                  ];
 
 
 export default class EditTest extends React.Component<ILayoutProps, ILayoutState> {
@@ -55,6 +58,7 @@ export default class EditTest extends React.Component<ILayoutProps, ILayoutState
       super(props);
       this.state = {
         _lesJoueurs: [],
+        _formState: 0
       }
   }
 
@@ -64,7 +68,10 @@ export default class EditTest extends React.Component<ILayoutProps, ILayoutState
     
     // Lorsque les joueurs sont chargés.
     Store.on("change", () => {
-      this.setState({ _lesJoueurs: Store.GetAllJoueurs() })
+      this.setState({ 
+        _lesJoueurs: Store.GetAllJoueurs(),
+        _formState: 0
+      })
       this.RemplirSelect();
     });
   }
@@ -128,14 +135,14 @@ export default class EditTest extends React.Component<ILayoutProps, ILayoutState
    * Ouvre le form d'ajout d'action
    */
   openActionForm = (e: React.MouseEvent<HTMLInputElement>, sender: HTMLButtonElement) => {
-    Actions.requestActionForm(e, sender, document.getElementById("Enr") as HTMLDivElement);
+    Actions.requestActionForm(e, sender, document.getElementsByClassName("Enr")[0] as HTMLDivElement);
   }
 
   /**
    * Ferme le form d'ajout d'action
    */
   closeActionForm = () => {
-    Actions.closeActionForm(document.getElementById("Enr") as HTMLDivElement);
+    Actions.closeActionForm(document.getElementsByClassName("Enr")[0] as HTMLDivElement);
   }
   //Envoie du formulaire à l'api 
   sendFormData(e: React.MouseEvent<HTMLInputElement>) {
@@ -181,6 +188,79 @@ export default class EditTest extends React.Component<ILayoutProps, ILayoutState
     }
   }
 
+  setTerrainFromInfo = () => {
+    // Définir la position initiale du joueur.
+  }
+
+  setTerrainToInfo = () => {
+    // Définir l'action finale du joueur.
+    this.setState({
+      _formState: 2,
+      _lesJoueurs: this.state._lesJoueurs
+    });
+  }
+
+  setActionFromInfo = () => {
+    // Affiche le terrain.
+    this.setState({
+      _formState: 1,
+      _lesJoueurs: this.state._lesJoueurs
+    });
+  }
+
+  setFromArrow = (e: React.MouseEvent<HTMLDivElement>) => {
+    fleche = [[e.nativeEvent.offsetX, e.nativeEvent.offsetY], fleche[1]];
+
+    // Effacer le canvas
+    var canvas = document.getElementById('canvasArrow') as HTMLCanvasElement;
+    canvas.width = canvas.width;
+  }
+
+  setToArrow = (e: React.MouseEvent<HTMLDivElement>) => {
+    fleche =  [fleche[0], [e.nativeEvent.offsetX, e.nativeEvent.offsetY]];
+
+    // Dessiner la flèche
+    var canvas = document.getElementById('canvasArrow') as HTMLCanvasElement;
+    var ctx = canvas.getContext('2d');
+
+    console.log(fleche);
+
+    let ajustement = 1.8;
+
+    ctx.strokeStyle = "blue";
+    ctx.fillStyle = "blue";
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.moveTo(fleche[0][0] / (ajustement - 0.7), fleche[0][1]  / ajustement);
+    ctx.lineTo(fleche[1][0] / (ajustement - 0.7), fleche[1][1]  / ajustement);
+    ctx.stroke();
+
+    var endRadians = Math.atan((fleche[1][1] - fleche[0][1]) / (fleche[1][0] - fleche[0][0]));
+    endRadians += ((fleche[1][0] > fleche[0][0]) ? 90 : -90) * Math.PI / 180;
+    this.drawArrowhead(ctx, fleche[1][0] / (ajustement - 0.7), fleche[1][1] / ajustement, endRadians);
+  }
+
+  drawArrowhead = (ctx: CanvasRenderingContext2D, x: number, y: number, radians: number) => {
+      ctx.save();
+      ctx.beginPath();
+      ctx.translate(x,y);
+      ctx.rotate(radians);
+      ctx.moveTo(0,0);
+      ctx.lineTo(5,20);
+      ctx.lineTo(-5,20);
+      ctx.closePath();
+      ctx.restore();
+      ctx.fill();
+  }
+
+  returnFirstStateForm = () => {
+    this.setState({
+      _formState: 0,
+      _lesJoueurs: this.state._lesJoueurs
+    })
+  }
+
   render() {
    
     var nbTempo =0;
@@ -197,22 +277,25 @@ export default class EditTest extends React.Component<ILayoutProps, ILayoutState
        */
       //let ligne = (this.state._lesJoueurs[i]["LastLignePlayed"] == "def" ? 0 : (this.state._lesJoueurs[i]["LastLignePlayed"] == "cen" ? 1 : 2));
       
-       let ligne = (nbTempo == 0 ? 0 :( nbTempo==2 ? 1 : 2));
+       let ligne = (nbTempo == 0 ? 0 :( nbTempo == 2 ? 1 : 2));
       /**
        * Obtenir la dernière position jouée (gauche, centre ou droite).
        */
       //let position = (this.state._lesJoueurs[i]["LastPositionPlayed"] == "gau" ? 0 : (this.state._lesJoueurs[i]["LastLignePlayed"] == "cen" ? 1 : 2));
       nbTempo++;
       rows[ligne][nbTempo2].push(<li>
-        <button 
+        <button
+          className="btn btn-primary"
           value={this.state._lesJoueurs[i]["Number"]} 
           onClick={this.openActionForm.bind(this)}>{this.state._lesJoueurs[i]["Number"]}
         </button></li>);
     } 
-    return (
-      <div>
-        <form onSubmit={this.sendFormData.bind(this)}>  
-          <div id="Enr">
+
+    // Définit le form
+    var formAction: any;
+    if (this.state._formState == 0) {
+      formAction = <form onSubmit={this.setActionFromInfo.bind(this)}>  
+          <div className="Enr">
             <button 
               type="button" 
               className="close" 
@@ -221,18 +304,102 @@ export default class EditTest extends React.Component<ILayoutProps, ILayoutState
               onClick={this.closeActionForm.bind(this)}>
               <span aria-hidden="true">&times;</span>
             </button>
-            <h3>enregistrer une action</h3><br></br>               
-            <label htmlFor="Nom">Nom de l'action</label>                  
-            <select id="NomActivite" name="NomActivite"></select><br></br>
-            <label htmlFor="resultat">Résultat de l'action</label>
-            <input type="radio" name="resultat" value="true" /> Reussi
-            <input type="radio" name="resultat" value="false"/> Manqué<br></br>
-            <input type="submit" value="Submit"  />    
-
+            <h3>Première action</h3><hr />
+            <div className="form-group">          
+              <label htmlFor="Nom">Nom de l'action</label>                  
+              <select id="NomActivite" className="form-control" name="NomActivite"></select><br></br>
+            </div>
+            <div className="form-group">
+              <label htmlFor="resultat">Résultat de l'action </label> 
+              <div className="radio">   
+                <label>
+                  <input type="radio" id="actionReussi" name="resultat" value="true" /> Reussi
+                </label>
+              </div>
+              <div className="radio">  
+                <label>
+                  <input type="radio" id="actionManque" name="resultat" value="false"/> Manqué<br></br>
+                </label>
+              </div>
+            </div>
+            <hr />
+            <div className="form-group col-xs-4 col-xs-push-8">
+              <input type="submit" className="btn btn-default" value="Trajectoire" />
+            </div>
           </div>  
-        </form> 
+        </form>;
+    } else if (this.state._formState == 1) {
+      formAction = <form onSubmit={this.setTerrainFromInfo.bind(this)}>
+        <div className="Enr">
+          <button 
+              type="button" 
+              className="close" 
+              data-dismiss="alert" 
+              aria-label="Close" 
+              onClick={this.closeActionForm.bind(this)}>
+              <span aria-hidden="true">&times;</span>
+          </button>
+          <h3>Définir la trajectoire</h3><hr />
+          <div id="terrain-container-sm" onMouseDown={this.setFromArrow.bind(this)} onMouseUp={this.setToArrow.bind(this)}> 
+            <div id="circle-centre"></div>
+            <div id="def-container" className="col-xs-12 col-sm-4 terrain-third"></div>
+            <div id="def-container" className="col-xs-12 col-sm-4 terrain-third"></div>
+            <div id="def-container" className="col-xs-12 col-sm-4 terrain-third"></div>
+            <canvas id="canvasArrow"></canvas>
+          </div>
+          <hr />
+          <div className="col-xs-2 no-l-padd">
+            <input onClick={this.returnFirstStateForm.bind(this)} className="btn btn-default" value="Retour" />
+          </div>
+          <div className="col-xs-6 col-xs-push-4">
+            <input onClick={this.setTerrainToInfo.bind(this)} className="btn btn-success" value="Action finale" />
+          </div>
+        </div>
+      </form>
+    } else {
+      formAction = <form onSubmit={this.setActionFromInfo.bind(this)}>  
+          <div className="Enr">
+            <button 
+              type="button" 
+              className="close" 
+              data-dismiss="alert" 
+              aria-label="Close" 
+              onClick={this.closeActionForm.bind(this)}>
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <h3>Action finale</h3><hr />
+            <div className="form-group">          
+              <label htmlFor="Nom">Nom de l'action</label>                  
+              <select id="NomActivite" className="form-control" name="NomActivite"></select><br></br>
+            </div>
+            <div className="form-group">
+              <label htmlFor="resultat">Résultat de l'action </label> 
+              <div className="radio">   
+                <label>
+                  <input type="radio" id="actionReussi" name="resultat" value="true" /> Reussi
+                </label>
+              </div>
+              <div className="radio">  
+                <label>
+                  <input type="radio" id="actionManque" name="resultat" value="false"/> Manqué<br></br>
+                </label>
+              </div>
+            </div>
+            <hr />
+            <div className="col-xs-2 no-l-padd">
+              <input onClick={this.setActionFromInfo.bind(this)} className="btn btn-default" value="Retour" />
+            </div>
+            <div className="col-xs-6 col-xs-push-4">
+              <input onClick={this.sendFormData.bind(this)} className="btn btn-success" value="Enregistrer" />
+            </div>
+          </div>  
+        </form>;
+    }
 
-        <input type="button"onClick={this.demi.bind(rows)} value="mi-Temps"/>
+    return (
+      <div>
+        {formAction}
+        <input type="button" onClick={this.demi.bind(rows)} value="mi-Temps"/>
         <form onSubmit={this.sendFormData.bind(this)}>  
             <h3>Pointage</h3><br></br>               
             <label htmlFor="ScoreDom">Domicile</label>
