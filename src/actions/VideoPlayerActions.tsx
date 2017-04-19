b9359492f0fb3b63d0d229b2b10d40fefcfbd6ef
Alexandrehebertvincent <alexandre.hebertvincent@gmail.com>
2017-04-18 21:27:49 -0400

    Parameters shorthand update

--- src/actions/VideoPlayerActions.tsx
+++ src/actions/VideoPlayerActions.tsx
@@ -1,95 +1,93 @@
 import dispatcher from "../dispatcher/dispatcher";
 
-// tslint:disable:object-literal-shorthand
-
 // Met le vidéo en mode lecture.
 export function playVideo(state: boolean, video: HTMLVideoElement) {
     dispatcher.dispatch({
-        state: state,
+        state,
         type: "VIDEO_PLAYER.PLAY_VIDEO",
-        video: video,
+        video,
     });
 }
 
 export function pauseVideo(state: boolean, video: HTMLVideoElement) {
     dispatcher.dispatch({
-        state: state,
+        state,
         type: "VIDEO_PLAYER.PAUSE_VIDEO",
-        video: video,
+        video,
     });
 }
 
 export function restart(video: HTMLVideoElement) {
     dispatcher.dispatch({
         type: "VIDEO_PLAYER.RESTART",
-        video: video,
+        video,
     });
 }
 
 export function backFive(video: HTMLVideoElement) {
     dispatcher.dispatch({
         type: "VIDEO_PLAYER.BACK_FIVE",
-        video: video,
+        video,
     });
 }
 
 export function forwardFive(video: HTMLVideoElement) {
     dispatcher.dispatch({
         type: "VIDEO_PLAYER.FORWARD_FIVE",
-        video: video,
+        video,
     });
 }
 
 export function stopVideo(state: boolean, video: HTMLVideoElement) {
     dispatcher.dispatch({
-        state: state,
+        state,
         type: "VIDEO_PLAYER.STOP_VIDEO",
-        video: video,
+        video,
     });
 }
 
 export function slideTime(video: HTMLVideoElement, slider: HTMLInputElement) {
     dispatcher.dispatch({
-        slider: slider,
+        slider,
         type: "VIDEO_PLAYER.SLIDE_TIME",
-        video: video,
+        video,
     });
 }
 
 export function videoPlaying(video: HTMLVideoElement, slider: HTMLInputElement) {
     dispatcher.dispatch({
-        slider: slider,
+        slider,
         type: "VIDEO_PLAYER.VIDEO_PLAYING",
-        video: video,
+        video,
     });
 }
 
 export function restoreDefaultSlowSliderValue(slider: HTMLInputElement) {
     dispatcher.dispatch({
-        slider: slider,
+        slider,
         type: "VIDEO_PLAYER.RESTORE_SLOW_SLIDER",
     });
 }
 
 export function slowSliderSlide(slider: HTMLInputElement, video: HTMLVideoElement) {
     dispatcher.dispatch({
-        slider: slider,
+        slider,
         type: "VIDEO_PLAYER.SLOW_SLIDER_SLIDE",
-        video: video,
+        video,
     });
 }
 
 export function setCurrentTime(time: number) {
     dispatcher.dispatch({
-        time: time,
+        time,
         type: "VIDEO_PLAYER.SET_CURRENT_TIME",
     });
 }
 
 export function setStepValues(stepInfo: HTMLSpanElement, slider: HTMLInputElement) {
     dispatcher.dispatch({
-        slider: slider,
-        stepInfo: stepInfo,
+        slider,
+        stepInfo,
         type: "VIDEO_PLAYER.SET_STEP_VALUE",
     });
 }
