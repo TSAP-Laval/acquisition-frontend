522c9cd1e9e21a422e17c380bc0291da8146c66f
Mehdi Laribi <m.laribi@hotmail.com>
2017-04-25 09:56:08 -0400

    JQuery removal

--- src/components/Manage/Actions.tsx
+++ src/components/Manage/Actions.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 import {Button, Alert} from "react-bootstrap";
-import * as $ from "jquery";
 
 import * as requesthandler from './RequestHandler';
 import actionStore from '../../stores/ActionsStore';
@@ -50,10 +49,15 @@ export default class Actions extends React.Component<ILayoutProps, ILayoutState>
     //Post action 
     SubmitAction(){
 
+        var Description = document.getElementById("action_desc") as HTMLInputElement;
+        var Acquisition = document.getElementById("acquisition") as HTMLSelectElement;
+        var Separation = document.getElementById("separation") as HTMLSelectElement;
+        debugger;
+
         var text = '{'
-       +'"Description" : '+ '"' +$('#action_desc').val() + '"' + ','
-       +'"Acquisition" : '+ '"' +$('#acquisition').val() + '"' + ','
-       +'"Separation" : '+ '"' +$('#separation').val() + '"'
+       +'"Description" : '+ '"' +Description.value+ '"' + ','
+       +'"Acquisition" : '+ '"' + Acquisition.value + '"' + ','
+       +'"Separation" : '+ '"' +Separation.value + '"'
        +'}';
        
        alert("Ajout Réussi");
@@ -80,7 +84,7 @@ export default class Actions extends React.Component<ILayoutProps, ILayoutState>
     //nouvellement ajoutée.
     AddNew(data:any)
     {
-            var doc = document.getElementsByClassName("action_table");
+            var table = document.getElementById('table_action');
 			  var x = document.createElement("tr");
 
               var tdesc =  document.createElement("td");
@@ -99,7 +103,7 @@ export default class Actions extends React.Component<ILayoutProps, ILayoutState>
               x.appendChild(tc);
               x.appendChild(tm);
 			  console.log(x);
-			  $('#action_table tbody').append(x);
+			  table.appendChild(x);
     }
     
     
@@ -113,36 +117,6 @@ export default class Actions extends React.Component<ILayoutProps, ILayoutState>
 
     render() {
         
-
-
-//Aucune row trouvée
-$(function(){
-    var $tbody = $('#action_table tbody');
-    var $rowCount = $('#action_table tr').length;
-    var warningTr =   "<tr id='noAction'><td>Aucune action n'a été trouvée</td></tr>"
-    var idWarning = $('#noAction');
-
-    if($rowCount== 0){
-
-        $tbody.append(warningTr);
-    }else{
-        $tbody.remove('#noAction');
-    }
-});
-
-
-    //Ajout d'une nouvelle action
-    function AddRow( actionDesc:string, separation:string, acquisition:string){
-        
-        
-         var trToAdd =   "<tr id='action1'><td>" + String(actionDesc) + "</td><td contenteditable='true'>" 
-                        + String(acquisition) + "</td><td>" 
-                        + String(separation) + "</td></tr>";
-
-            $('#action_table tbody').append(trToAdd)
-
-            $('#action_desc').val("");
-    }
 
         return (
 
